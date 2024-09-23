9c334a6c7568bc26d9b1409325fa471757d24a82
jurek.ohrndorf <jurek.ohrndorf@check24.de>
2024-09-23 16:22:13 +0200

    socket

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -11,7 +11,9 @@ const props = defineProps<{
 }>();
 
 // on my server VITE_API_BASE_URL is: https://auju.org
-const socket = io(import.meta.env.VITE_API_BASE_URL + "/chat/api");
+const socket = io(import.meta.env.VITE_API_BASE_URL + "/chat/api", {
+  path: "/chat/api/socket.io",
+});
 
 const messageInput = ref<HTMLDivElement | null>(null);
 const messages = ref<messageGroupI[]>([]);
